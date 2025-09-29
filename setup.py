1bcb7ada68585931bfcd8de4216d4cb93ead095b
Abhishek <abhishek.bhade@galaxeye.space>
2025-09-29 15:12:04 +0530

    Apply Black formatting & normalize line endings

--- setup.py
+++ setup.py
@@ -3,10 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="enlarge",
     version="0.1.0",
-    description=(
-        "A simple package to duplicate an image "
-        "into two identical copies"
-    ),
+    description=("A simple package to duplicate an image " "into two identical copies"),
     author="Your Name",
     author_email="your.email@example.com",
     license="MIT",
